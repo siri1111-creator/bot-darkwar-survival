7de029ac325793da3626854e67b09e0c7fb1b46c
Jirawatp058 <jirawat.p058@gmail.com>
2026-03-13 22:48:02 +0700

    bug

--- darkwar-zombie.py
+++ darkwar-zombie.py
@@ -114,7 +114,7 @@ def attack_zombie_routine():
                     print("[+] กดรวมพล")
                     
                     # 5. เช็คว่าพลังศัตรูตีไหวไหม
-                    if find('good_enermy.png'):
+                    if find('images/good_enermy.png'):
                         print("[+] ศัตรูพลังน้อยกว่า")
                         
                         # 6. กดปุ่มเตรียมส่งทัพ
